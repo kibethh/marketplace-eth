c0d745fe9f60c7eadd638afa60dd68405e037cce
kibethh <kibethh@gmail.com>
2022-08-26 20:29:11 +0300

    separated creation of courseHash

--- components/providers/web3/hooks/useNetwork.js
+++ components/providers/web3/hooks/useNetwork.js
@@ -1,5 +1,5 @@
-import { useEffect } from "react"
-import useSWR from "swr"
+import { useEffect } from "react";
+import useSWR from "swr";
 
 const NETWORKS = {
   1: "Ethereum Main Network",
@@ -9,32 +9,36 @@ const NETWORKS = {
   42: "Kovan Test Network",
   56: "Binance Smart Chain",
   1337: "Ganache",
-}
+};
 
-const targetNetwork = NETWORKS[process.env.NEXT_PUBLIC_TARGET_CHAIN_ID]
+const targetNetwork = NETWORKS[process.env.NEXT_PUBLIC_TARGET_CHAIN_ID];
 
 export const handler = (web3, provider) => () => {
-
-  const { data, mutate, ...rest } = useSWR(() =>
-    web3 ? "web3/network" : null,
+  const { data, mutate, ...rest } = useSWR(
+    () => (web3 ? "web3/network" : null),
     async () => {
-      const chainId = await web3.eth.getChainId()
-      return NETWORKS[chainId]
+      const chainId = await web3.eth.getChainId();
+
+      if (!chainId) {
+        throw new Error("Cannot retrieve a network!");
+      }
+
+      return NETWORKS[chainId];
     }
-  )
+  );
 
   useEffect(() => {
     provider &&
-    provider.on("chainChanged", chainId => {
-      mutate(NETWORKS[parseInt(chainId, 16)])
-    })
-  }, [web3])
+      provider.on("chainChanged", (chainId) => {
+        mutate(NETWORKS[parseInt(chainId, 16)]);
+      });
+  }, [web3]);
 
   return {
     data,
     mutate,
     target: targetNetwork,
     isSupported: data === targetNetwork,
-    ...rest
-  }
-}
+    ...rest,
+  };
+};
--- pages/marketplace/courses/owned.js
+++ pages/marketplace/courses/owned.js
@@ -6,29 +6,44 @@ import { MarketHeader } from "@components/ui/marketplace";
 import { getAllCourses } from "@content/courses/fetcher";
 import { useRouter } from "next/router";
 import Link from "next/link";
+import { useWeb3 } from "@components/providers";
 
 export default function OwnedCourses({ courses }) {
   const router = useRouter();
   const { account } = useAccount();
   const { ownedCourses } = useOwnedCourses(courses, account.data);
+  const { requireInstall } = useWeb3();
 
   return (
     <>
       <MarketHeader />
       <section className="grid grid-cols-1">
-        {ownedCourses.hasInitialResponse &&
-          (!ownedCourses.data || ownedCourses?.data.length === 0) && (
-            <div className="w-1/2">
-              <Message type="warning">
-                <div>You don't own any courses!</div>
-                <Link href="/marketplace">
-                  <a className="font-normal hover:underline">
-                    <i>Purchase course</i>
-                  </a>
-                </Link>
-              </Message>
-            </div>
-          )}
+        {ownedCourses.isEmpty && (
+          <div className="w-1/2">
+            <Message type="warning">
+              <div>You don't own any courses!</div>
+              <Link href="/marketplace">
+                <a className="font-normal hover:underline">
+                  <i>Purchase course</i>
+                </a>
+              </Link>
+            </Message>
+          </div>
+        )}
+        {account.isEmpty && (
+          <div className="w-1/2">
+            <Message type="warning">
+              <div>Please connect to metamask!</div>
+            </Message>
+          </div>
+        )}
+        {requireInstall && (
+          <div className="w-1/2">
+            <Message type="warning">
+              <div>Please install metamask!</div>
+            </Message>
+          </div>
+        )}
 
         {ownedCourses.data?.map((course) => (
           <OwnedCourseCard key={course.id} course={course}>
