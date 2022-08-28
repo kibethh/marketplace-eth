e338ae2c7ce24184c93279413ce91ad6db8f1893
kibethh <kibethh@gmail.com>
2022-08-28 11:23:29 +0300

    deleted hardhat compiled artifacts and added more tests

--- test/courseMarketplace.test.js
+++ test/courseMarketplace.test.js
@@ -1,4 +1,6 @@
 const CourseMarketplace = artifacts.require("CourseMarketplace");
+const { assert } = require("chai");
+const { catchRevert } = require("./utils/exceptions");
 
 contract("CourseMarketplace", (accounts) => {
   const courseId = "0x00000000000000000000000000003130";
@@ -8,6 +10,7 @@ contract("CourseMarketplace", (accounts) => {
 
   let _contract = null;
   let contractOwner = null;
+  let newOwner = null;
   let buyer = null;
   let courseHash = null;
 
@@ -15,6 +18,7 @@ contract("CourseMarketplace", (accounts) => {
     _contract = await CourseMarketplace.deployed();
     contractOwner = accounts[0];
     buyer = accounts[1];
+    newOwner = accounts[2];
   });
 
   describe("Purchase new course", () => {
@@ -24,6 +28,14 @@ contract("CourseMarketplace", (accounts) => {
         value,
       });
     });
+    it("should not repurchased course similar course", async () => {
+      await catchRevert(
+        _contract.purchaseCourse(courseId, proof, {
+          from: buyer,
+          value,
+        })
+      );
+    });
     it("Can get the purchased course hash by index", async () => {
       const index = 0;
       courseHash = await _contract.getCourseHashAtIndex(index);
@@ -32,7 +44,12 @@ contract("CourseMarketplace", (accounts) => {
         { type: "address", value: buyer }
       );
 
-      expect(courseHash).to.be.equal(expectedHash);
+      // expect(courseHash).to.be.equal(expectedHash);
+      assert.equal(
+        courseHash,
+        expectedHash,
+        "Purchased course cannot be got by index"
+      );
     });
 
     it("should match the data of the course purchased by buyer", async () => {
@@ -67,14 +84,11 @@ contract("CourseMarketplace", (accounts) => {
 
   describe("Can activate purchased course", () => {
     it("should not be able to activate course by non other than contract owner", async () => {
-      try {
-        await _contract.activateCourse(courseHash, {
+      await catchRevert(
+        _contract.activateCourse(courseHash, {
           from: buyer,
-        });
-        assert(false);
-      } catch (error) {
-        assert(error, "expected an error but didn't get one");
-      }
+        })
+      );
     });
     it("should have a state of 'activated'", async () => {
       await _contract.activateCourse(courseHash, {
@@ -88,6 +102,56 @@ contract("CourseMarketplace", (accounts) => {
         course.state,
         expectedState,
         "course state not matching 'activated' state"
+      );
+    });
+  });
+
+  describe("Can transfer ownership", () => {
+    let currentOwner = null;
+    before(async () => {
+      currentOwner = await _contract.getContractOwner();
+    });
+
+    it("get contract owner", async () => {
+      assert.equal(
+        contractOwner,
+        currentOwner,
+        "Current owner isn't the deployer"
+      );
+    });
+
+    it("Allows Only contract owner to transfer contract ownership ", async () => {
+      await catchRevert(
+        _contract.transferOwnerShip(newOwner, {
+          from: buyer,
+        })
+      );
+    });
+    it("Allows owner be transfered ", async () => {
+      await _contract.transferOwnerShip(newOwner, {
+        from: contractOwner,
+      });
+
+      const owner = await _contract.getContractOwner();
+
+      assert.equal(
+        owner,
+        newOwner,
+        "Current owner should be equal to the new owner"
+      );
+    });
+
+    it("transfers ownership back to original contract owner ", async () => {
+      const owner = await _contract.getContractOwner();
+      await _contract.transferOwnerShip(contractOwner, {
+        from: owner,
+      });
+      const originalOwner = await _contract.getContractOwner();
+
+      assert.equal(
+        originalOwner,
+        contractOwner,
+        "Current owner should be the original contract owner"
       );
     });
   });
