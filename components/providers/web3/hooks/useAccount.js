b8119e768bded2453cb3381fbf2b29f548b975aa
kibethh <kibethh@gmail.com>
2022-08-30 15:10:10 +0300

    added deployment to ropsten

--- components/providers/web3/hooks/useAccount.js
+++ components/providers/web3/hooks/useAccount.js
@@ -3,6 +3,7 @@ import useSWR from "swr";
 
 const adminAddresses = {
   "0xb00d71c576241cbe12a3f7798f3939ba7e426400a530c6f7ae1ce82f2cf613ca": true,
+  "0x8662625fe18b19921cd6218474a73ac77d6104ccea04318063f68901e5a16968": true,
 };
 
 export const handler = (web3, provider) => () => {
